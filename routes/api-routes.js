55d048e822644f5473050fa7e27e7ae96575972c
Tyler Murton <tylermurton@gmail.com>
2020-07-18 11:38:09 -0400

    lines to separate comments

--- routes/api-routes.js
+++ routes/api-routes.js
@@ -1,8 +1,8 @@
 const router = require('express').Router();
-const comment = require("../models/index.js");
+const Comment = require("../models/index.js");
 const db = require('../models/index.js');
 
-console.log("This is comment: ", comment)
+console.log("This is comment: ", Comment)
 
 
 
@@ -43,18 +43,18 @@ router.get('/scores', (req, res) => {
 
 
   // Get all comments
-  // router.get("/scores", function(req, res) {
+  router.get("/scores", function(req, res) {
 
-  //   // Finding all comments, and then returning them to the user as JSON.
-  //   // Sequelize queries are asynchronous, which helps with perceived speed.
-  //   // If we want something to be guaranteed to happen after the query, we'll use
-  //   // the .then function
-  //   db.Comments.findAll({}).then(function(results) {
-  //     // results are available to us inside the .then
-  //     res.json(results);
-  //   });
+    // Finding all comments, and then returning them to the user as JSON.
+    // Sequelize queries are asynchronous, which helps with perceived speed.
+    // If we want something to be guaranteed to happen after the query, we'll use
+    // the .then function
+    db.Comment.findAll({}).then(function(results) {
+      // results are available to us inside the .then
+      res.json(results);
+    });
 
-  // });
+  });
 
   // // Add a comment
   router.post("/scores", function(req, res) {
@@ -62,7 +62,7 @@ router.get('/scores', (req, res) => {
     console.log("Comment Data:");
     console.log(req.body);
 
-    newComment.create({
+    db.Comment.create({
       author: req.body.author,
       body: req.body.body,
       created_at: req.body.created_at
